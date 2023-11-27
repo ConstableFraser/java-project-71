4af3b12034db47a1e3d74b1b85b1eddadf40501a
sanswed <sanswed@gmail.com>
2023-11-28 00:12:12 +0300

    step #4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,23 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "path to first file")
+    private static String filePath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private static String filePath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private static String format;
     @Override
     public Integer call() {
         return 0;
@@ -14,6 +25,6 @@ public class App implements Callable<Integer> {
 
     public static void main(String[] args) {
         new CommandLine(new App()).execute(args);
-        Differ.generate();
+        Differ.generate(format, filePath1, filePath2);
     }
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
 public class Differ {
-    public static void generate() {
-        //System.out.println("::called method Differ.generate()::");
+    public static void generate(String format, String filePath1, String filePath2) {
+        System.out.println("result: " + format + " " + filePath1 + " " + filePath2);
     }
 }
